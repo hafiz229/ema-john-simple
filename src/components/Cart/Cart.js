3e2a40be0c858edc592fa02993846891a0a9298b
Hafiz <hasnathafiz229>
2021-10-08 01:20:08 +0600

    useProducts

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,19 +3,24 @@ import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
-  const totalReducer = (previous, product) => previous + product.price;
-  const total = cart.reduce(totalReducer, 0);
-  /* let total = 0;
+  /* const totalReducer = (previous, product) => previous + product.price;
+  const total = cart.reduce(totalReducer, 0); */
+  let totalQuantity = 0;
+  let total = 0;
   for (const product of cart) {
-    total = total + product.price;
-  } */
+    if (!product.quantity) {
+      product.quantity = 1;
+    }
+    total = total + product.price * product.quantity;
+    totalQuantity = totalQuantity + product.quantity;
+  }
   const shipping = total > 0 ? 15 : 0;
   const tax = (total + shipping) * 0.1;
   const grandTotal = total + shipping + tax;
   return (
     <div>
       <h3>Order Summary</h3>
-      <h5>Items Ordered: {props.cart.length}</h5>
+      <h5>Items Ordered: {totalQuantity}</h5>
       <br />
       <p>Total: {total.toFixed(2)}</p>
       <p>Shipping: {shipping}</p>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,11 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
+import Rating from "react-rating";
 
 const Product = (props) => {
   console.log(props.product);
-  const { name, img, seller, price, stock } = props.product;
+  const { name, img, seller, price, stock, star } = props.product;
   const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
   return (
     <div className="product">
@@ -21,6 +22,13 @@ const Product = (props) => {
         <p>
           <small>only {stock} left in the stock - order soon</small>
         </p>
+        <Rating
+          initialRating={star}
+          emptySymbol="far fa-star icon-color"
+          fullSymbol="fas fa-star icon-color"
+          readonly
+        ></Rating>
+        <br />
         <button
           onClick={() => props.handleAddToCart(props.product)}
           className="btn-regular"
