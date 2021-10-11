e345af7892ef0ec404a9b971ecd8438a1cc277ec
Hafiz <hasnathafiz229>
2021-10-12 03:23:01 +0600

    item remove done

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,32 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const OrderReview = () => {
+  const [products] = useProducts();
+  const [cart, setCart] = useCart(products);
+  const handleRemove = (key) => {
+    const newCart = cart.filter((product) => product.key !== key);
+    setCart(newCart);
+    removeFromDb(key);
+  };
   return (
-    <div>
-      <h1>This is Order Review</h1>
+    <div className="shop-container">
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem
+            product={product}
+            key={product.key}
+            handleRemove={handleRemove}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
